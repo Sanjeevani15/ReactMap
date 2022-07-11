68fb8c662864174e1985ea5fd7ce511304f67100
Sanjeevani Bhandari <sanjeevanibhandari@Sanjeevani-Bhandari-I156s-MacBook-Pro.local>
2022-07-11 15:50:24 +0530

    dynamically showing marker using GEOLOCATION API and later making the marker draggable

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,61 +1,38 @@
-import React,{useState} from 'react';
-import { GoogleMap, LoadScript,Marker,InfoWindow } from '@react-google-maps/api';
+import React,{useEffect,useState} from 'react';
+import { GoogleMap, LoadScript,Marker } from '@react-google-maps/api';
 
 const MapContainer = () => {
   
-    const [ selected, setSelected ] = useState({});
+    const [ currentPosition, setCurrentPosition ] = useState({});
   
-    const onSelect = item => {
-      setSelected(item);
-    }
+    const success = position => {
+      const currentPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      setCurrentPosition(currentPosition);
+    };
+    
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition(success);
+    })
 
+    const onMarkerDragEnd = (e) => {
+        const lat = e.latLng.lat();
+        const lng = e.latLng.lng();
+        setCurrentPosition({ lat, lng})
+      };
 
   const mapStyles = {        
     height: "100vh",
     width: "100%"
 };
   
-  const defaultCenter = {
-    lat: 28.644800, lng: 77.216721
-  }
+//   const defaultCenter = {
+//     lat: 28.644800, lng: 77.216721
+//   }
   
-  const locations = [
-    {
-      name: "Location 1",
-      location: { 
-        lat: 41.3954,
-        lng: 2.162 
-      },
-    },
-    {
-      name: "Location 2",
-      location: { 
-        lat: 41.3917,
-        lng: 2.1649
-      },
-    },
-    {
-      name: "Location 3",
-      location: { 
-        lat: 41.3773,
-        lng: 2.1585
-      },
-    },
-    {
-      name: "Location 4",
-      location: { 
-        lat: 41.3797,
-        lng: 2.1682
-      },
-    },
-    {
-      name: "Location 5",
-      location: { 
-        lat: 41.4055,
-        lng: 2.1915
-      },
-    }
-  ];
+  
 
   return (
      <LoadScript
@@ -63,27 +40,24 @@ const MapContainer = () => {
         <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={3}
-          center={defaultCenter}>
-        {
-            locations.map(item => {
-              return (
-                <Marker key={item.name}  position={item.location} onClick={() => onSelect(item)}
-              />
-              )
-            })
-         }
-         {
-            selected.location && 
-            (
-              <InfoWindow
-              position={selected.location}
-              clickable={true}
-              onCloseClick={() => setSelected({})}
-            >
-              <p>{selected.name}</p>
-            </InfoWindow>
-            )
-         }
+        //   center={currentPosition}>
+        //   {
+        //     currentPosition.lat &&
+        //     ( 
+        //       <Marker position={currentPosition}/>
+        //     ) 
+        //   }
+        
+        center={currentPosition}>
+          {
+            currentPosition.lat ? 
+            <Marker
+            position={currentPosition}
+            onDragEnd={(e) => onMarkerDragEnd(e)}
+            draggable={true} /> :
+            null
+          }
+          
           </GoogleMap>
      </LoadScript>
   )
